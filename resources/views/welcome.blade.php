24ac2c0d881234844bef471c2dce2c12a7c841e7
Unknown <carlosdavid_229@hotmail.com>
2018-03-10 11:56:24 -0500

    Cambios grandes
    Se hicieron varios cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,28 +2,28 @@
 
 @section('contenido')
    <font color="black" face="Lithos Pro">
-			<br><br>
 			<div class="container">
-			  
-			  	<blockquote>
-			    <div class="col"><h1>Buscador de personas</h1>
-			    </blockquote>
-			    	
+			<center>
+				<font color="Cyan" face="Lithos Pro"><h1>Buscador de personas</h1></font>
 					  <form action="{{url('formulario')}}" method="POST">
 					  	{{csrf_field()}}
-					  	
-					  	<label for="buscador">Condiciones: </label><br>
-					    <select class="custom-select my-1 mr-sm-2" class="border border-warning" name="buscador">
+							
+					  	<img src="imagen/datos1.png" width=35%>
+							<br>
+					  	<label for="buscador"><H2>Condiciones: </H2></label><br>
+					    <H4><select class="custom-select my-1 mr-sm-2" class="border border-warning" name="buscador">
 					      <option>Nombres</option>
 					      <option>Apellidos</option>
 					      <option>Edad</option>
 					      <option>Pais</option>
 					      <option>Profesion</option>
 					    </select>	
+							</H4>
 					  	<br><br>
-					  	<input type="text" name="datoBusqueda" class="border border-warning" class="rounded">
-					  	<br><br><button type="submit" class="btn btn-danger">Send</button>
+					  	<H2><input type="text" name="datoBusqueda" class="border border-warning" class="rounded">
+							<br><br><button type="submit" class="btn btn-warning btn-lg">Send</button></H2>
 					  </form>
+						</center>
 			</div>
 			</font>
 

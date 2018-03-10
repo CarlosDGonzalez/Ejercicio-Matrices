f5e8cd08b2a07ea1de2cb9b9a2d0e71bec8e1df2
Unknown <carlosdavid_229@hotmail.com>
2018-03-10 12:08:45 -0500

    Habia error
    Habia un error y estoy haciedo cambios

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,36 +1,46 @@
-<?php
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		
+		<title> Inicio </title>
+		<!-- css boostrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <meta charset="utf-8">
+		  <meta name="viewport" content="width=device-width, initial-scale=1">
+		  
+	</head>
 
-namespace App\Http\Controllers;
+	<body background="imagen/fondo.jpg">
+			<br>
+			<br>
+			<center>
+			<font color="Yellow" face="Lithos Pro">
+			<h1>
+			<b> 
+				<?php 
+					echo "Ejercicio Buscador Personas";
+				?>
+			</b>
+			</h1>
+			</font>
+			</center>
+			<br>
+		<div class="container-fluid">
+		 	
+			@yield('contenido')
+			
+		</div>
+		
+		<!-- js boostrap -->
+		 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	</body>
 
-use Illuminate\Http\Request;
 
-class LogicaMatrizController extends Controller
-{
-    
-    public function gestionMatriz(Request $request)
-    {
-        $datoBusqueda = $request->input('datoBusqueda');
-	    	$buscador = $request->input('buscador');
-	    	$bandera=0;
-	        
-	    	$datos=[];
-	    	$datos[]=['nombres'=>'Carlos','apellidos'=>'Gonzalez','edad'=>'17','pais'=>'Colombia','profesion'=>'Estudiante'];
-	    	$datos[]=['nombres'=>'Juan','apellidos'=>'Hernandez','edad'=>'34','pais'=>'Venezuela','profesion'=>'Estudiante'];
-	    	$datos[]=['nombres'=>'Manuel','apellidos'=>'Martinez','edad'=>'18','pais'=>'Colombia','profesion'=>'Bombero'];
-	    	$datos[]=['nombres'=>'Carlos','apellidos'=>'Garcia','edad'=>'22','pais'=>'Chile','profesion'=>'Bombero'];
-	    	$datos[]=['nombres'=>'Hugo','apellidos'=>'Gordillo','edad'=>'45','pais'=>'Argentina','profesion'=>'Profesor'];
-	    	$datos[]=['nombres'=>'Dora','apellidos'=>'Bernal','edad'=>'47','pais'=>'USA','profesion'=>'Astronauta'];
-	    	$datos[]=['nombres'=>'Camilo','apellidos'=>'Gonzalez','edad'=>'23','pais'=>'Mexico','profesion'=>'Policia'];
-	    	$datos[]=['nombres'=>'Tatiana','apellidos'=>'Pachon','edad'=>'19','pais'=>'España','profesion'=>'Comerciante'];
-	    	$datos[]=['nombres'=>'Jose','apellidos'=>'Benitez','edad'=>'72','pais'=>'Italia','profesion'=>'Comerciante'];
-	    	$datos[]=['nombres'=>'Natalia','apellidos'=>'Velasquez','edad'=>'17','pais'=>'Francia','profesion'=>'Estudiante'];
-	      
-	    	$info=[
-	    		'datos'=>$datos,
-	    		'buscador'=>$buscador,
-	    		'datoBusqueda'=>$datoBusqueda
-	    	];
-	    	
-	    return view('contenido',$info);
-	}
-}
+</html>
